27463eec1949aa718845a52aec8ddba6ba1cf9e5
Назим Малышев <nazimmalysev@MacBook-Pro-Nazim.local>
2024-10-03 00:42:52 -0700

    done

--- equations.fsx
+++ equations.fsx
@@ -1,28 +1,54 @@
 // Определите функции для решение алгебраических уравнений
 
-let dichotomy f a b = 0.
+let eps = 1e-4
+let sgn a = if a > 0. then 1. else if a < 0. then -1. else 0.
 
-let iterations phi x0 = 0.
+let rec dichotomy f a b =
+    if sgn(f a) = sgn(f b) then
+        b
+    else
+        let c = (a + b) / 2. 
+        if f c = 0. || (c - a) < eps then
+            c
+        else if ((f a) * (f c) < 0.) then
+            dichotomy f a c
+        else
+            dichotomy f c b
 
-let newthon f f' x0 = 0.
-// используйте функцию 'iterations'
+let rec iterations phi x0 = 
+    if abs (x0 - (phi x0)) < eps then
+        x0
+    else
+        let next = phi x0
+        iterations phi next
+let newthon f f' x0 = 
+    let phi x : float = x - (f x) / (f' x)
+    iterations phi x0
 
 // Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+let f1 x = tan (x / 2.) - (1. / tan (x / 2.)) + x
+let f2 x = 0.4 + atan(sqrt(x)) - x 
+let f3 x = 3. * sin(sqrt(x))  + 0.35 * x - 3.8
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f1' x = 1. /  (2. *  cos ((x / 2.) ** 2.)) + 1. /  (2. *  sin ((x / 2.) ** 2)) + 1.
+let f2' x = 1. / (2. * sqrt(x) + 2. * x * sqrt(x)) - 1.
+let f3' x = (3. * cos(sqrt(x))) / (2. * sqrt(x)) + 0.35
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let phi1 x = - tan (x / 2.) + (1. / tan (x / 2.))
+let phi2 x = 0.4 + atan(sqrt(x))
+let phi3 x = (-3. * sin(sqrt(x)) + 3.8) / 0.35
 
 let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
 
- 
+    printfn "---------------------------------------------------------------------------"
+    printfn "|    f    |  a  |  b  |    Diho    |    Iter    |   Nueton   |  Apr  Val  |"
+    printfn "---------------------------------------------------------------------------"
+    printfn "|    1    |  1  |  2  |  %10.6f|  %10.6f|  %10.6f|  %10.6f|" (dichotomy f1 1. 2.) (iterations phi1 1.5) (newthon f1 f1' 1.5) 1.0769
+    printfn "---------------------------------------------------------------------------" 
+    printfn "|    1    |  1  |  2  |  %10.6f|  %10.6f|  %10.6f|  %10.6f|" (dichotomy f2 1. 2.) (iterations phi2 1.5) (newthon f2 f2' 1.5) 1.2388
+    printfn "---------------------------------------------------------------------------" 
+    printfn "|    1    |  1  |  2  |  %10.6f|  %10.6f|  %10.6f|  %10.6f|" (dichotomy f3 1. 2.) (iterations phi3 2.5) (newthon f3 f3' 2.5) 2.2985
+    printfn "---------------------------------------------------------------------------" 
+
+
+main
--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,52 @@
-// Print a table of a given function f, computed by taylor series
+let eps = 1e-7
 
-// function to compute
-let f = sin
+let builtin x = 
+  let chisl = 2. * x - 3.
+  let znam = (x - 1.) ** 2.
+  let res = chisl / znam
+  res
 
-let a = 0.0
-let b = 1.0
-let n = 10
+let get_taylor_naive_part x n = 
+  let res = - (n + 3.) * x ** n
+  res
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let taylor_naive x = 
+  let mutable sum = 0.0
+  let mutable curr_elem = get_taylor_naive_part x 0
+  let mutable iters = 0
+  while abs(curr_elem) > eps do
+      sum <- sum + curr_elem
+      iters <- iters + 1
+      curr_elem <- get_taylor_naive_part x iters
+  (sum, iters)
 
+let get_taylor_smart_part x n = 
+  let chisl = (n + 3.) * x
+  let znam = n + 2.
+  let res = chisl / znam
+  res
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+let taylor_smart x = 
+  let mutable sum = 0.0
+  let mutable curr_elem = -3.
+  let mutable iters = 0
+  while abs(curr_elem) > eps do
+      sum <- sum + curr_elem
+      iters <- iters + 1
+      curr_elem <- curr_elem * get_taylor_smart_part x iters
+  (sum, iters)
 
 let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+    let a = 0.1
+    let b = 0.6
+    let n = 10
+    printfn "---------------------------------------------------------------------------------"
+    printfn "|  x  |    Builtin   |  Smart Taylor|    #terms     |   Dumb Taylor|     #terms |"
+    printfn "---------------------------------------------------------------------------------"
+    for i = 0 to n do
+        let x = a + (float i) / (float n) * (b - a)
+        printfn "|%5.2f|  %10.6f  |  %10.6f  |   %10d  |  %10.6f  |    %10d  |" 
+            x (builtin x) (fst (taylor_naive x)) (snd (taylor_naive x )) (fst (taylor_smart x)) (snd (taylor_smart x)) 
+    printfn "----------------------------------------------------------------------------"
 
 main
-
